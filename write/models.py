f8c947254dc4cc065779b0f6d729c9b82e8499f9
kcocu <kcocu@naver.com>
2022-12-30 11:45:15 +0900

    11

--- write/models.py
+++ write/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Good(models.Model):
+    good_id = models.AutoField(primary_key=True)
+    free = models.ForeignKey(Free, on_delete=models.CASCADE,db_column='free_id', null=True)
+    id = models.ForeignKey(Info, on_delete=models.CASCADE,db_column='info_id', null=True)
+    join = models.ForeignKey(Join, on_delete=models.CASCADE,db_column='join_id', null=True)
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE,db_column='gallery_id', null=True)
