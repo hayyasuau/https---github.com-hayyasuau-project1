db7ebea819b20c08fcca7269bb5aa73a2e9c31ed
kcocu <kcocu@naver.com>
2022-12-30 14:54:00 +0900

    123231

---
+++ write/forms.py
@@ -0,0 +1,62 @@
+from django import forms
+from .models import Free
+from django_summernote.fields import SummernoteTextField
+from django_summernote.widgets import SummernoteWidget
+
+class BoardWriteForm(forms.ModelForm):
+    title = forms.CharField(
+        label='글 제목'
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'게시글 제목'
+            }
+        )
+    )
+    contents = SummernoteTextField
+
+
+    #여기 아래에 select로 게시판 종류 추가하기
+    options = (
+        ('Free','자유게시판')
+        ('Free2','자유게시판2')
+        ('Free3','자유게시판3')
+
+    )
+
+    board_name = forms.ChoiceField(
+        label='게시판 선택'
+        widget=forms.Select(),
+        choices=options
+    )
+
+    field_order  = [
+        'title',
+        'text',
+        'info_id'
+    ]
+
+    class Meta:
+        model = Free
+        fields = [
+            'title',
+            'text',
+            'info_id'
+        ]
+        widgets = {
+            'contents' : SummernoteWidget()
+        }
+    def clean(self):
+        cleaned_data = super().clean()
+
+        title = cleaned_data.get('title','')
+        text = cleaned_data.get('text','')
+        info = cleaned_data.get('info_id','Free')
+
+        if title == '':
+            self.add_error('title','글 제목을 입력하세요.')
+        elif contents =='':
+            self.add_error('text','글 내용을 입력하세요')
+        else:
+            self.title = title
+            self.text = text
+            self.info = info
--- write/views.py
+++ write/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .forms import BoardWriteForm
+from .models import Free
+from all_info.models import Info
 
 # Create your views here.
 def board_list(request):
@@ -9,6 +12,32 @@ def board_list(request):
 def board_free_write(request):
     login_session = request.session.get('login_session','')
     context = {'login_session' : login_session}
+    if request.method == 'GET' :
+        write_form = BoardWriteForm
+        context['forms'] = write_form
+        return render(request, 'write/board_free_write.html', context)
+    
+    elif request.method =='POST':
+        write_form = BoardWriteForm(request.POST)
+
+        if write_form.is_valid():
+            writer = Info.objects.get(user_id=login_session)
+            board = Free(
+                title=write_form.title,
+                texts=write_form.text,
+                writer=writer
+                board_name=write_form.info
+            )
+            board.save()
+            return redirect('/free_write')
+        
+        else:
+            context['forms'] = write_form
+            if write_form.errors:
+                for value in write_form.errors.values():
+                    context['error'] = value
+            return render(request, 'write/board_free_write.html',context)
+
     return render(request, 'wirte/board_free_write.html', context)
 
 def free(request):
