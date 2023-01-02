d526bbcc5c190c77c98ed6f52f3bc6cd3ad4eb56
kcocu <kcocu@naver.com>
2023-01-02 17:56:18 +0900

    update,delete,댓글 조금

--- make_moim/urls.py
+++ make_moim/urls.py
@@ -7,5 +7,4 @@ app_name = 'make_moim'
 
 urlpatterns = [
     path('', views.make_moim, name='make_moim'),
-    path('detail/<int:pk>', views.make_detail, name='make_detail'),
 ]
--- select_moim/views.py
+++ select_moim/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 from django.views.generic import DetailView
+from make_moim.models import Make_Moim
 from select_moim.models import Select_Moim
+from write.models import Good
 
 # Create your views here.
 def select_moim(request):
@@ -16,3 +19,45 @@ def select_moim(request):
 
 # class Select_DetailView(DetailView):
 
+def update(request, id):
+    make_moim=Make_Moim.objects.get(id=id)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        commend = request.POST.get('commend')
+        location = request.POST.get('location')
+        imgfile = request.POST.get('imgfile')
+        max_people = request.POST.get('max_people')
+        
+        try : 
+            make_moim.name = name
+            make_moim.commend = commend
+            make_moim.location = location
+            make_moim.imgfile = imgfile
+            make_moim.max_people = max_people
+            make_moim.save()
+            return render(request,'make_moim/detail.html')
+        except:
+            return redirect('/select_moim')
+
+def delete(request, id):
+    try :
+        make_moim=Make_Moim.objects.get(id=id)
+        make_moim.delete()
+        return render(request,'make_moim/detail.html')
+    except:
+            return redirect('/select_moim')
+
+        
+def make_detail(request, id):
+    try :
+        make_moim= Make_Moim.objects.get(id=id)
+        good=Good.objects.filter(make_moim=make_moim)
+
+        context = {
+            'make_moim' : make_moim,
+            'good' : good
+            
+        }
+    except Make_Moim.DoesNotExist:
+        raise Http404('해당 게시물을 찾을 수 없습니다.')
+    return render(request, 'make_moim/make_detail.html', context)
